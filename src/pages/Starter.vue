48903dfad577d263cfc41c98858d2d8816675292
Oriol <oriol@funda.nl>
2021-03-26 15:43:27 +0100

    Initial commit

---
+++ src/pages/Starter.vue
@@ -0,0 +1,114 @@
+<template>
+  <div>
+    <div class="page-header page-header-small">
+      <parallax
+        class="page-header-image"
+        style="background-image:url('img/cvt/nevatmin.jpg')"
+      >
+      </parallax>
+      <div class="content-center">
+        <div class=" container">
+          <h1 class="h1-seo title">SERVEIS CLÍNICS VETERINARIS</h1>
+          <h3>AMB ELS MILLORS PROFESSIONALS</h3>
+          <div class="text-center">
+            <a href="#pablo" class="btn btn-primary btn-icon btn-round">
+              <i class="fab fa-facebook-square"></i>
+            </a>
+            <a href="#pablo" class="btn btn-primary btn-icon btn-round">
+              <i class="fab far fa-envelope"></i>
+            </a>
+            <a href="#pablo" class="btn btn-primary btn-icon btn-round">
+              <i class="fab fas fa-phone"></i>
+            </a>
+          </div>
+          <n-button type="primary" round>Contacta'ns</n-button>
+        </div>
+      
+      </div>
+    </div>
+<div class="main">
+      <div class="section section-images">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+               <alert type="info" >
+                  <div class="alert-icon">
+                  </div>
+                  <strong>Clínica veterinaria: Des del 1999 tenint cura de les vostres mascotes.</strong>
+                </alert>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <basic-elements></basic-elements>
+    <tabs-section></tabs-section>
+    <progress-pagination></progress-pagination>
+    <notifications></notifications>
+    <typography></typography>
+    <javascript-components></javascript-components>
+    <carousel-section></carousel-section>
+    <nucleo-icons-section></nucleo-icons-section>
+    <div class="section">
+      <div class="container text-center">
+        <div class="row justify-content-md-center">
+          <div class="col-md-12 col-lg-8">
+            <h2 class="title">Completed with examples</h2>
+            <h5 class="description">
+              The kit comes with three pre-built pages to help you get started
+              faster. You can change the text and images and you're good to go.
+              More importantly, looking at them will give you a picture of what
+              you can built with this powerful Bootstrap 4 ui kit.
+            </h5>
+          </div>
+        </div>
+      </div>
+    </div>
+    <signup-form></signup-form>
+    <examples-section></examples-section>
+    <download-section></download-section>
+  </div>
+</template>
+<script>
+import { Parallax } from '@/components';
+import BasicElements from './components/BasicElementsSection';
+import TabsSection from './components/Tabs';
+import ProgressPagination from './components/ProgressPagination';
+import Notifications from './components/Notifications';
+import Typography from './components/Typography';
+import JavascriptComponents from './components/JavascriptComponents';
+import CarouselSection from './components/CarouselSection';
+import NucleoIconsSection from './components/NucleoIconsSection';
+import SignupForm from './components/SignupForm';
+import ExamplesSection from './components/ExamplesSection';
+import DownloadSection from './components/DownloadSection';
+import {
+  Button,
+  Alert
+} from '@/components';
+export default {
+  name: 'index',
+  bodyClass: 'index-page',
+  components: {
+    Parallax,
+    BasicElements,
+    TabsSection,
+    ProgressPagination,
+    Notifications,
+    Typography,
+    JavascriptComponents,
+    CarouselSection,
+    NucleoIconsSection,
+    SignupForm,
+    ExamplesSection,
+    DownloadSection,
+    [Button.name]: Button,
+    Alert
+  }
+};
+</script>
+<style>
+.starter-page {
+  min-height: calc(100vh - 95px);
+}
+</style>
